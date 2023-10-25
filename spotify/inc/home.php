fe1b3261e708030892c6550287eb288ab780885c
Hanbi Hanz Choi <personabe1984@gmail.com>
2023-10-25 19:45:06 -0400

    testing

--- spotify/inc/home.php
+++ spotify/inc/home.php
@@ -8,7 +8,7 @@
 
 // Include page header
 include 'inc/html/header.php';
-$REDIRECT_URI = "https://cantio.live/spotify/callback/index.php";
+$REDIRECT_URI = "https://cantio.live/spotify/callback/callback.php";
 $CLIENT_ID = "446002b2d4434bff9fec8de86cee969d";
 $CLIENT_SECRET = "645c16a2a1bd4a1990df607ccc7e1b17";
 ?>
