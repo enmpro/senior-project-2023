b1c4e94a4b3919202497f2dbf2b15c2decab0c18
Enmanuel Proano <rbsrman123@gmail.com>
2023-10-31 11:02:50 -0400

    moved some, edits to profile edit

---
+++ profile_edit.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'logindb.php';
+
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
+
