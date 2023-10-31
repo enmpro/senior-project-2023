2e796a9ba22336b40de4c2b23ebd54dae400242b
Enmanuel Proano <rbsrman123@gmail.com>
2023-10-31 11:34:06 -0400

    test

--- profile_edit.php
+++ profile_edit.php
@@ -9,11 +9,7 @@ try {
 }
 
 session_start();
-if (!isset($_SESSION['user_id'])) {
-    // The user is not logged in, redirect them to the login page
-    header('Location: main.php');
-    exit;
-}
+
 
 $username = $_SESSION['user_id'];
 $user_id = $_SESSION['user_num'];
