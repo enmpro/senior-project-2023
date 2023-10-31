7e914bac738e3b56d20dd18c14fe8aa27e2fa710
Enmanuel Proano <rbsrman123@gmail.com>
2023-10-30 23:53:47 -0400

    fixed redirects

--- community.php
+++ community.php
@@ -15,12 +15,12 @@ $username = $_SESSION['user_id'];
 
 
 <!DOCTYPE html>
-<html data-bs-theme="light" style="font-size: 14px;">
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Homepage</title>
+    <title>Community</title>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Latest compiled JavaScript -->
@@ -28,7 +28,20 @@ $username = $_SESSION['user_id'];
 </head>
 
 <body>
-
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <p class="navbar-brand">
+                CANTIO
+            </p>
+            <a href="homepage.php">Home</a>
+            <a href="#">About</a>
+            <a href="profile.html">Profile</a>
+            <a href="community.php">Community</a>
+        </div>
+    </nav>
+    <div>
+        <p>THIS IS WHERE THE COMMUNITY PAGE IS</p>
+    </div>
 </body>
 
 </html>
