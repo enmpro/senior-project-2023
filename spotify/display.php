6819505c05046d2c40638602fff4a46e90e3ca46
Hanbi Hanz Choi <personabe1984@gmail.com>
2023-10-31 11:47:52 -0400

    testing

--- spotify/display.php
+++ spotify/display.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'logindb.php';
 
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
 session_start();
 $access_token = $_SESSION['access_token'];
 
