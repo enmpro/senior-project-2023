4fb759f69e9f117b47d0d13cc58523cfd6f801e1
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-01 20:52:49 -0400

    test

--- profile_edit.php
+++ profile_edit.php
@@ -64,7 +64,7 @@ function alter_profile($pdo, $description, $userid) {
                    WHERE UserID = :userid";
     $stmtProfile = $pdo->prepare($editProfile);
 
-    $stmtProfile->bindParam(':newDesc', $description, PDO::PARAM_STR);
+    $stmtProfile->bindParam(':newDesc', $description, PDO::PARAM_STR, 50);
     $stmtProfile->bindParam(':userid', $userid, PDO::PARAM_STR, 11);
     
     $stmtProfile->execute();
