42008d82dfc09e1a95c5ab98851e6fb0320b7028
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-02 12:56:24 -0400

    test

--- upload.php
+++ upload.php
@@ -2,51 +2,70 @@
 if (isset($_POST["submit"])) {
     // Check if a file was uploaded without errors
     if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] == 0) {
-        $localFile = $_FILES["fileToUpload"]["tmp_name"]; // Temporary uploaded file
-        $fileName = $_FILES["fileToUpload"]["name"]; // Original file name
-
-        echo $localFile. " ";
-        echo $fileName;
 
         $ftpServer = "ftp.cantio.live";
         $ftpUsername = "enmanuel@cantio.live";
         $ftpPassword = "VsCfKTDy9m@N";
 
-        // Connect to the FTP server
-        $ftpConnection = ftp_connect($ftpServer);
-        if ($ftpConnection === false) {
-            die("Could not connect to the FTP server");
-        } else {
-            echo "connected";
+        $remote_dir = '/public_html/userphoto/';
+        $src_file = $_FILES["fileToUpload"]["name"]; // Original file name
+
+        if ($src_file != '') {
+            // remote file path
+            $dst_file = $remote_dir . $src_file;
+
+            // connect ftp
+            $ftpcon = ftp_connect($ftpServer) or die('Error connecting to ftp server...');
+
+            // ftp login
+            $ftplogin = ftp_login($ftpcon, $ftpUsername, $ftpPassword);
+
+            // ftp upload
+            if (ftp_put($ftpcon, $dst_file, $src_file, FTP_ASCII))
+                echo 'File uploaded successfully to FTP server!';
+            else
+                echo 'Error uploading file! Please try again later.';
+
+            // close ftp stream
+            ftp_close($ftpcon);
         }
 
-        $login_result = ftp_login($ftpConnection, $ftpUsername, $ftpPassword);
-        // Login to the FTP server
-        if (!$login_result) {
-            die("FTP login failed");
-        } else {
-            echo "connected";
-        }
 
-        // Set transfer mode to binary for image files
-        ftp_set_option($ftpConnection, FTP_BINARY, true);
+        // // Connect to the FTP server
+        // $ftpConnection = ftp_connect($ftpServer);
+        // if ($ftpConnection === false) {
+        //     die("Could not connect to the FTP server");
+        // } else {
+        //     echo "connected";
+        // }
 
-        $remoteDirectory = "/public_html/userphoto/"; // Replace with the remote directory path
+        // $login_result = ftp_login($ftpConnection, $ftpUsername, $ftpPassword);
+        // // Login to the FTP server
+        // if (!$login_result) {
+        //     die("FTP login failed");
+        // } else {
+        //     echo "connected";
+        // }
 
-        // Generate a unique remote file name using timestamp
-        $remoteFile = $remoteDirectory . time() . "_" . $fileName;
+        // // Set transfer mode to binary for image files
+        // ftp_set_option($ftpConnection, FTP_BINARY, true);
 
-        echo $remoteFile;
+        // $remoteDirectory = "/public_html/userphoto/"; // Replace with the remote directory path
 
-        // Upload the file to the FTP server
-        if (ftp_put($ftpConnection, $remoteFile, $localFile, FTP_BINARY)) {
-            echo "File uploaded successfully";
-        } else {
-            echo "File upload failed";
-        }
+        // // Generate a unique remote file name using timestamp
+        // $remoteFile = $remoteDirectory . time() . "_" . $fileName;
 
-        // Close the FTP connection
-        ftp_close($ftpConnection);
+        // echo $remoteFile;
+
+        // // Upload the file to the FTP server
+        // if (ftp_put($ftpConnection, $remoteFile, $localFile, FTP_BINARY)) {
+        //     echo "File uploaded successfully";
+        // } else {
+        //     echo "File upload failed";
+        // }
+
+        // // Close the FTP connection
+        // ftp_close($ftpConnection);
     } else {
         echo "Error: " . $_FILES["fileToUpload"]["error"];
     }
