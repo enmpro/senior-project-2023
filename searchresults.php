1ad865cd81f4ce26a6638c3e8b5b017e4cff7f54
Nicole <vierne@farmingdale.edu>
2023-11-01 20:53:49 -0400

    changes

--- searchresults.php
+++ searchresults.php
@@ -1,30 +1,28 @@
 <?php
 session_start(); 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $search_term = $_POST['search_term'];
+if (isset($_POST['search_term']) && !empty($_POST['search_term'])) {
+    $search_term = htmlspecialchars($_POST['search_term']);
+
     
     include 'logindb.php'; #the file with the database connection
 
     #queries the database to search for other users
-    $sql = "SELECT * FROM User WHERE Username LIKE '%$search_term%'";
+    $sql = "SELECT * FROM User WHERE Username LIKE '%$search_term%'"; /
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<h3>{$row['Username']}</h3>";
-            echo "<p>Email: {$row['Email']}</p>";
-            echo "<p>FirstName: {$row['FirstName']}</p>";
-            echo "<p>LastName: {$row['LaststName']}</p>";
-            echo "<p>Zip: {$row['Zip']}</p>";
+
+        while($row = $result->fetch_assoc()) {
+            echo "Username: " . $row['Username'] . "<br>";
         }
     } else {
-        echo "No matching users found.";
+        echo "No results found";
     }
 
     $conn->close();
 } else {
-    header("Location: profile.php");
-    exit();
+    echo "Please enter a search term";
 }
 ?>
