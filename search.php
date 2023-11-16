b3a76d55ee5537d672279cf68ab89112342726b3
Nicole <vierne@farmingdale.edu>
2023-11-16 00:09:25 -0500

    minor changes

--- search.php
+++ search.php
@@ -8,17 +8,14 @@
 <h1>Search Results</h1>
 
 <?php
-require_once 'login.php';
+    require_once 'login.php';
     session_start();
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $search_term = $_POST['search_term'];
-
         $search_term = htmlspecialchars($_POST['search_term']);
-    }
-    include 'logindb.php'; #database connection file
 
-    $sql = "SELECT * FROM Users WHERE Username LIKE '%$search_term%'";
-    $result = $conn->query($sql);
+        $sql = "SELECT * FROM Users WHERE Username LIKE '%$search_term%'";
+        $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
@@ -28,9 +25,12 @@ require_once 'login.php';
     else {
         echo "No matching users found.";
     }
+}
     else {
         header("Location: searchusers.html");
+        exit();
     }
+
 
     $conn->close();
 ?>
