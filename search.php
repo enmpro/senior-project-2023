b9ef450fc1c6e8b6efa6f6046a645b622a41d0b4
Nicole <vierne@farmingdale.edu>
2023-11-15 21:21:13 -0500

    changed

--- search.php
+++ search.php
@@ -8,6 +8,11 @@
 <h1>Search Results</h1>
 
 <?php
+    session_start();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $search_term = $_POST['search_term'];
+
+    include 'logindb.php';
 ?>
 
 </body>
