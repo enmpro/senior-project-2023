cb2262d073087837aae0762b3963b580c5cde283
Nicole <vierne@farmingdale.edu>
2023-11-15 22:34:39 -0500

    session

--- search.php
+++ search.php
@@ -12,7 +12,24 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $search_term = $_POST['search_term'];
 
-    include 'logindb.php';
+    include 'logindb.php'; #database connection file
+
+    $sql = "SELECT * FROM Users WHERE Username LIKE '%$search_term%'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "Username: " . $row['Username'] . "<br>";
+        }
+    }
+    else {
+        echo "No matching users found.";
+    }
+    else {
+        header("Location: searchusers.html");
+    }
+
+    $conn->close();
 ?>
 
 </body>
