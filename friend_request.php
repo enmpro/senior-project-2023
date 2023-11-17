4da30b320667de093418e9a9027a8527b384a530
Nicole <vierne@farmingdale.edu>
2023-11-17 16:54:28 -0500

    created

---
+++ friend_request.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'logindb.php';
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the username from the form
+$username = $_POST['username'];
+
+// Insert the friend request into the database
+$sql = "INSERT INTO Users (Username) VALUES ('$username')";
+$conn->query($sql);
+
+// Close the database connection
+$conn->close();
+
+// Redirect back to the main page
+header("Location: index.html");
+exit();
+?>
