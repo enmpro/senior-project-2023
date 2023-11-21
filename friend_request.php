74d36d969ab0f2985c2d0b7635f1083957d59c34
Nicole <vierne@farmingdale.edu>
2023-11-20 20:33:59 -0500

    test

--- friend_request.php
+++ friend_request.php
@@ -9,16 +9,32 @@ if ($conn->connect_error) {
 }
 
 #takes the username from the form (friend_request.html)
-$username = $_POST['username'];
+$friend_username = $_POST['friend_username'];
 
-#inserts the friend request into the database
-$sql = "INSERT INTO Users (Username) VALUES ('$username')";
-$conn->query($sql);
+$sender_user_id = 1;
 
+$result = $conn->query("SELECT Username FROM Users WHERE Username = '$friend_username'");
+
+if ($result->num_rows > 0) {
+    // Friend found, get their user ID
+    $row = $result->fetch_assoc();
+    $friend_user_id = $row['id'];
+
+    // Check if a friend request already exists
+    $existingRequest = $conn->query("SELECT * FROM friend_requests WHERE sender_id = $sender_user_id AND receiver_id = $friend_user_id");
+
+    if ($existingRequest->num_rows === 0) {
+        // No existing request, send a new friend request
+        $conn->query("INSERT INTO friend_requests (sender_id, receiver_id, status) VALUES ($sender_user_id, $friend_user_id, 'pending')");
+
+        echo "Friend request sent successfully!";
+    } else {
+        echo "Friend request already sent!";
+    }
+} else {
+    // Friend not found
+    echo "Friend not found in the database!";
+}
 #database connection closes
 $conn->close();
-
-#takes user back to the homepage
-header("Location: homepage_page.html");
-exit();
 ?>
