5153cf1017cb001dcab28fd15741e9afe22c4f28
Nicole <vierne@farmingdale.edu>
2023-11-20 21:33:59 -0500

    changes

---
+++ reject_request.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'logindb.php';
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the ID of the friend request being rejected
+$request_id = $_GET['id'];
+
+// Update the friend request status to 'rejected'
+$conn->query("UPDATE friend_requests SET status = 'rejected' WHERE id = $request_id");
+
+// Close the database connection
+$conn->close();
+
+// Redirect back to the main page
+header("Location: index.html");
+exit();
+?>
