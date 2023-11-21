2556b40454ee7357e43ceb610a41adfab168d1ce
Nicole <vierne@farmingdale.edu>
2023-11-21 12:13:51 -0500

    will fix later

--- accept_request.php
+++ accept_request.php
@@ -5,18 +5,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-#accepted friend request id
-$request_id = $_GET['id'];
+// #accepted friend request id
+// $request_id = $_GET['id'];
 
-#changes status to "accepted
-$conn->query("UPDATE FriendRequest SET Status = 'accepted' WHERE id = $request_id");
+// #changes status to "accepted
+// $conn->query("UPDATE FriendRequest SET Status = 'accepted' WHERE id = $request_id");
 
-#fetches sender id
-$request_info = $conn->query("SELECT sender_id FROM friend_requests WHERE id = $request_id")->fetch_assoc();
-$sender_user_id = $request_info['sender_id'];
+// #fetches sender id
+// $request_info = $conn->query("SELECT sender_id FROM friend_requests WHERE id = $request_id")->fetch_assoc();
+// $sender_user_id = $request_info['sender_id'];
 
-#adds user to friends table
-$conn->query("INSERT INTO friends (user1_id, user2_id) VALUES ($sender_user_id, $receiver_user_id)");
+// #adds user to friends table
+// $conn->query("INSERT INTO friends (user1_id, user2_id) VALUES ($sender_user_id, $receiver_user_id)");
 
 #database connection is closed
 $conn->close();
