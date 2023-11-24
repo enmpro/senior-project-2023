e9d9aacf3a70b73cb106f957f615d671b98f65ef
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-24 16:27:23 -0500

    test

--- user_event.php
+++ user_event.php
@@ -29,6 +29,7 @@ $username = $_SESSION['user_name'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/fe58b05d68.js" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Latest compiled JavaScript -->
