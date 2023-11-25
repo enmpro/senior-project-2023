6c17c4b2ec229542aab4498184d942e6a1e36175
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-25 00:17:44 -0500

    test

--- user_event.php
+++ user_event.php
@@ -104,61 +104,76 @@ $username = $_SESSION['user_name'];
                 </div>
         
                 <!-- Add more music events as needed -->
-        
-                </div>
-                </div>
-
-                <div class="modal fade" id="eventModal$count" tabindex="-1" aria-labelledby="eventModalLabel$count" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="eventModalLabel$count">$event_Name</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <img src="$event_Photo" class="modal-img" alt="Concert in the Park">
-                                <p class="card-text row g-2">
-                                    <span class="col-auto" style="font-size: 16px;"><i class='fas fa-calendar-alt'></i></span>
-                                    <span class="col">Date: $event_DateTime</span>
-                                </p>
-                                <p class="card-text row g-2">
-                                    <span class="col-auto" style="font-size: 14px;"><i class='fas fa-location'></i></span>
-                                    <span class="col">Location: Arena Stadium, Los Angeles (Placeholder text)</span>
-                                </p>
-                                <p class="card-text row g-2">
-                                    <span class="col-auto" style="font-size: 14px;"><i class='fas fa-music'></i></span>
-                                    <span class="col">Artist: $event_Artist</span>
-                                </p>
-                                <p class="card-text">
-                                <div class="d-flex ">
-                                    <div class="pe-1">
-                                        <p>
-                                            Description:
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <p>
-                                            $event_Desc
-                                        </p>
-                                    </div>
-                                </div>
-                                </p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">RSVP</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    
                 _END;
 
             }
+
             ?>
 
-            <!-- Latest compiled JavaScript -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        </div>
+    </div>
+    <?php
+
+    foreach ($result as $row) {
+
+
+        echo <<<_END
+
+
+    
+        <div class="modal fade" id="eventModal$count" tabindex="-1" aria-labelledby="eventModalLabel$count"
+        aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="eventModalLabel$count">$event_Name</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="$event_Photo" class="modal-img" alt="Concert in the Park">
+                        <p class="card-text row g-2">
+                            <span class="col-auto" style="font-size: 16px;"><i class='fas fa-calendar-alt'></i></span>
+                            <span class="col">Date: $event_DateTime</span>
+                        </p>
+                        <p class="card-text row g-2">
+                            <span class="col-auto" style="font-size: 14px;"><i class='fas fa-location'></i></span>
+                            <span class="col">Location: Arena Stadium, Los Angeles (Placeholder text)</span>
+                        </p>
+                        <p class="card-text row g-2">
+                            <span class="col-auto" style="font-size: 14px;"><i class='fas fa-music'></i></span>
+                            <span class="col">Artist: $event_Artist</span>
+                        </p>
+                        <p class="card-text">
+                        <div class="d-flex ">
+                            <div class="pe-1">
+                                <p>
+                                    Description:
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    $event_Desc
+                                </p>
+                            </div>
+                        </div>
+                        </p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">RSVP</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    _END;
+    }
+
+
+    ?>
+
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
