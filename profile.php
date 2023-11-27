064722b025cb55fbc19a48c756d500e495f91753
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-26 20:13:36 -0500

    test

--- profile.php
+++ profile.php
@@ -19,6 +19,7 @@ if ($row = $result->fetch()) {
   $gender = $row['Gender'];
   $zip = $row['Zip'];
   $birthday = $row['Birthday'];
+  $userEmail = $row['Email'];
 }
 
 $query2 = "SELECT * FROM Profile WHERE UserID LIKE $user_id";
@@ -36,7 +37,7 @@ $query3 = "SELECT * FROM EventOrganizer WHERE UserID LIKE $user_id";
 $result3 = $pdo->query($query3);
 
 if ($row3 = $result3->fetch()) {
-    $organizerBool = true;
+  $organizerBool = true;
 } else {
   $organizerBool = false;
 }
@@ -56,7 +57,7 @@ if ($row3 = $result3->fetch()) {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Latest compiled and minified CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-  
+  <link rel="stylesheet" href="profile.css">
 </head>
 
 <body>
@@ -78,15 +79,18 @@ if ($row3 = $result3->fetch()) {
           <li class="nav-item">
             <a class="nav-link" href="community.php">Community</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="user_event.php">Events</a>
+          </li>
           <?php
-                        if ($organizerBool) {   
-                            echo <<<_END
-                            <li class="nav-item">
-                                <a class="nav-link" href="event_coord.php">Event Coordinator</a>
-                            </li>
-                            _END;
-                        }
-                    ?>
+          if ($organizerBool) {
+            echo <<<_END
+                    <li class="nav-item">
+                        <a class="nav-link" href="event_coord.php">Event Coordinator</a>
+                    </li>
+                    _END;
+          }
+          ?>
           <div>
             <form method="post" action="user_logout.php">
               <button type="submit" name="logout">Log Out</button>
@@ -96,6 +100,103 @@ if ($row3 = $result3->fetch()) {
       </div>
     </div>
   </nav>
+
+  <div class="container profile-container">
+    <div class="text-center">
+      <img src="modern-blue-dots-circle-background-vector.jpg" alt="Profile Picture" class="profile-picture">
+    </div>
+    <div class="user-info">
+      <h2><?php echo $fullname; ?> | <?php echo $username; ?></h2>
+      <?php
+      $query4 = "SELECT * FROM EventOrganizer WHERE UserID LIKE $user_id";
+      $result4 = $pdo->query($query4);
+      if ($row3 = $result3->fetch()) {
+        $organizerName = $row3['OrganizerName'];
+        $organizerType = $row3['OrganizerType'];
+        $organizerAddress = $row3['Address'];
+        $organizerPhone = $row3['Phone'];
+        $organizerEmail = $row3['ContactEmail'];
+        $organizerUrl = $row3['WebsiteURL'];
+        echo <<<_END
+            <h4><i>Event Organizer</i></h4>
+          _END;
+      }
+      ?>
+      <div class="mt-3">
+        <p>Email</p>
+        <p><?php echo $userEmail; ?></p>
+      </div>
+      <div>
+        <p>Location</p>
+        <p><?php echo $zip; ?></p>
+      </div>
+    </div>
+
+    <div class="text-center">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#organizerModal">Organizer
+        Information</button>
+    </div>
+
+    <hr>
+
+    <div class="additional-details text-center">
+      <h3>Additional Details</h3>
+      <div class="mt-3">
+        <p><strong>Age</strong></p>
+        <p><?php echo $birthday; ?></p>
+      </div>
+      <div>
+        <p><strong>Occupation</strong></p>
+        <p>Web Developer</p>
+      </div>
+      <div>
+        <p><strong>Description</strong></p>
+        <p><?php echo $description; ?></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="organizerModal" tabindex="-1" aria-labelledby="organizerModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="organizerModalLabel">Event Organizer Information</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div>
+            <p><strong>Organization Name</strong></p>
+            <p><?php echo $organizerName; ?></p>
+          </div>
+          <div>
+            <p><strong>Type</strong></p>
+            <p><?php echo $organizerType; ?></p>
+          </div>
+          <div>
+            <p><strong>Address</strong></p>
+            <p><?php echo $organizerAddress; ?></p>
+          </div>
+          <div>
+            <p><strong>Phone</strong></p>
+            <p><?php echo $organizerPhone; ?></p>
+          </div>
+          <div>
+            <p><strong>Email</strong></p>
+            <p><?php echo $organizerEmail; ?></p>
+          </div>
+          <div>
+            <p><strong>Website</strong></p>
+            <p><a href=""><?php echo $organizerUrl; ?></a></p>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn contact-btn">
+            Contact Organizer
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div class="card container-fluid w-75 mt-5">
     <h1><?php echo $username; ?></h1>
@@ -204,8 +305,8 @@ if ($row3 = $result3->fetch()) {
     <a class="btn btn-primary submit-btn" href="profile_edit.html">Edit Profile</a>
   </div>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
