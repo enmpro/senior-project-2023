51b13b258c87875962cfb21223f1c1d916634af7
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-27 19:40:28 -0500

    test

--- searchusers.php
+++ searchusers.php
@@ -1,6 +1,11 @@
 <?php
-require_once 'login.php';
+require_once 'logindb.php';
 
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
 
 session_start();
 if (!isset($_SESSION['user_name'])) {
