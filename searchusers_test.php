fadae3204792a83d08914b1caa676c8f953498ea
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-27 23:54:15 -0500

    test

--- searchusers_test.php
+++ searchusers_test.php
@@ -30,9 +30,52 @@ function test_userinput($data)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>User Search</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">CANTIO</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="homepage.php">Main</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="community.php">Community</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="user_event.php">Event</a>
+          </li>
+          <?php
+          if ($organizerBool) {
+            echo <<<_END
+                    <li class="nav-item">
+                        <a class="nav-link" href="event_coord.php">Event Coordinator</a>
+                    </li>
+                    _END;
+          }
+          ?>
+
+        </ul>
+        <div>
+          <form method="post" action="user_logout.php">
+            <button class="btn btn-secondary" type="submit" name="logout">Log Out</button>
+
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </nav>
 
     <h2>User Search</h2>
 
@@ -60,6 +103,7 @@ function test_userinput($data)
 
     
     ?>
-
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
