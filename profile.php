8332210cb4e3cb2f4ebeeba38568b79036eee5c2
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-27 23:23:25 -0500

    test

--- profile.php
+++ profile.php
@@ -1,12 +1,20 @@
 <?php
-require_once 'login.php';
+require_once 'logindb.php';
+
+try {
+  $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+  throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
 
 session_start();
 if (!isset($_SESSION['user_name'])) {
-  // The user is not logged in, redirect them to the login page
-  header('Location: landing.html');
-  exit;
+    // The user is not logged in, redirect them to the login page
+    header('Location: landing.html');
+    exit;
 }
+
 
 $username = $_SESSION['user_name'];
 $user_id = $_SESSION['user_id'];
