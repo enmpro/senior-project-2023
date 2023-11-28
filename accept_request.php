963bbf6636467fb2bfdca768f4d4d94662961425
Nicole <vierne@farmingdale.edu>
2023-11-27 19:58:07 -0500

    pdo things

--- accept_request.php
+++ accept_request.php
@@ -1,6 +1,19 @@
 <?php
 require_once 'logindb.php';
 
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
+session_start();
+if (!isset($_SESSION['user_name'])) {
+    // The user is not logged in, redirect them to the login page
+    header('Location: landing.html');
+    exit;
+}
+
 // Get the ID of the friend request being accepted
 $request_id = $_GET['id'];
 
