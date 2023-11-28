2ebeb47bbd42a579107ef2f6c5dcb8256265a183
Nicole <vierne@farmingdale.edu>
2023-11-27 21:10:09 -0500

    table change

--- accept_request.php
+++ accept_request.php
@@ -18,18 +18,16 @@ if (!isset($_SESSION['user_name'])) {
 $request_id = $_GET['id'];
 
 // Update the friend request status to 'accepted'
-$conn->query("UPDATE friend_requests SET status = 'accepted' WHERE id = $request_id");
+$conn->query("UPDATE FriendRequest SET status = 'accepted' WHERE id = $request_id");
 
 // Fetch the sender and receiver IDs
-$request_info = $conn->query("SELECT sender_id, receiver_id FROM friend_requests WHERE id = $request_id")->fetch_assoc();
+$request_info = $conn->query("SELECT sender_id, receiver_id FROM FriendRequest WHERE id = $request_id")->fetch_assoc();
 $sender_user_id = $request_info['sender_id'];
 $receiver_user_id = $request_info['receiver_id'];
 
 // Add the users to the friends table (you might need to adjust this based on your database structure)
 $conn->query("INSERT INTO friends (user1_id, user2_id) VALUES ($sender_user_id, $receiver_user_id)");
 
-// Close the database connection
-$conn->close();
 
 // Redirect back to the main page
 header("Location: homepage_page.html");
