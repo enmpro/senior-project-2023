bcc613eb05bc1087192ac73f035a07595d7dfdcb
Enmanuel Proano <rbsrman123@gmail.com>
2023-11-28 21:07:24 -0500

    test

---
+++ spotify/app.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+
+// Fetch the saved access token from somewhere. A session for example.
+$api->setAccessToken($accessToken);
+
+// It's now possible to request data about the currently authenticated user
+print_r(
+    $api->me()
+);
+
+// Getting Spotify catalog data is of course also possible
+print_r(
+    $api->getTrack('7EjyzZcbLxW7PaaLua9Ksb')
+);
+
+
+?>
---
+++ spotify/callbk.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$session = new SpotifyWebAPI\Session(
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'REDIRECT_URI'
+);
+
+$state = $_GET['state'];
+
+// Fetch the stored state value from somewhere. A session for example
+
+if ($state !== $storedState) {
+    // The state returned isn't the same as the one we've stored, we shouldn't continue
+    die('State mismatch');
+}
+
+// Request a access token using the code from Spotify
+$session->requestAccessToken($_GET['code']);
+
+$accessToken = $session->getAccessToken();
+$refreshToken = $session->getRefreshToken();
+
+// Store the access and refresh tokens somewhere. In a session for example
+
+// Send the user along and fetch some data!
+header('Location: app.php');
+die();
+
+?>
