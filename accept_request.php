4ef312fbf9813cd0ae4a29e444aa4dbeaff097de
Nicole <vierne@farmingdale.edu>
2023-11-30 21:20:18 -0500

    comment old code out

--- accept_request.php
+++ accept_request.php
@@ -21,21 +21,21 @@ function test_userinput($data)
     return $data;
 }
 // Get the ID of the friend request being accepted
-$request_id = $_GET['UserID'];
+// $request_id = $_GET['UserID'];
 
-// Update the friend request status to 'accepted'
-$sql("UPDATE FriendRequest SET status = 'accepted' WHERE UserID = $request_id");
+// // Update the friend request status to 'accepted'
+// $sql("UPDATE FriendRequest SET status = 'accepted' WHERE UserID = $request_id");
 
-// Fetch the sender and receiver IDs
-$request_info = $sql("SELECT RequestSend, RequestReceive FROM FriendRequest WHERE UserID = $request_id")->fetch_assoc();
-$sender_user_id = $request_info['RequestSend'];
-$receiver_user_id = $request_info['RequestReceive'];
+// // Fetch the sender and receiver IDs
+// $request_info = $sql("SELECT RequestSend, RequestReceive FROM FriendRequest WHERE UserID = $request_id")->fetch_assoc();
+// $sender_user_id = $request_info['RequestSend'];
+// $receiver_user_id = $request_info['RequestReceive'];
 
-// Add the users to the friends table (you might need to adjust this based on your database structure)
-//$conn->query("INSERT INTO friends (user1_id, user2_id) VALUES ($sender_user_id, $receiver_user_id)");
+// // Add the users to the friends table (you might need to adjust this based on your database structure)
+// //$conn->query("INSERT INTO friends (user1_id, user2_id) VALUES ($sender_user_id, $receiver_user_id)");
 
 
-// Redirect back to the main page
-header("Location: homepage_page.html");
-exit();
+// // Redirect back to the main page
+// header("Location: homepage_page.html");
+// exit();
 ?>
