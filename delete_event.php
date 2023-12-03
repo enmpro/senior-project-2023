76e1fb9fd9513d01ec8e855d59b113e51fc108a5
Enmanuel Proano <rbsrman123@gmail.com>
2023-12-03 16:14:12 -0500

    test

--- delete_event.php
+++ delete_event.php
@@ -8,10 +8,26 @@ try {
     throw new PDOException($e->getMessage(), (int) $e->getCode());
 }
 
+
+session_start();
+if (!isset($_SESSION['user_name'])) {
+    // The user is not logged in, redirect them to the login page
+    header('Location: landing.html');
+    exit;
+}
+
+if (isset($_SESSION['user_id'])) {
+
+    $userID = $_SESSION['user_id'];
+}
+
 $eventID = $_POST['id'];
 
 $query = "DELETE FROM Event WHERE EventID = $eventID";
 $result = $pdo->query($query);
+
+$deleteSql = "DELETE FROM UserRSVP WHERE EventID = $eventID AND UserID = $userID";
+$deleteRsvp = $pdo->prepare($deleteSql);
 
 header('Location: event_coord.php');
 
--- event_coord.php
+++ event_coord.php
@@ -123,27 +123,36 @@ if ($rowCheck = $resultCheck->fetch()) {
                         </div>
                         <div class="modal-body">
                             <form action="create_event.php" method="post" enctype="multipart/form-data">
-                                <div class="m-3">
-                                    <label for="eventName">Event Name</label>
-                                    <input id="eventName" name="eventName" type="text" required />
+                                <div class="mb-3">
+                                    <label for="eventName" class="form-label">Event Name</label>
+                                    <input name="eventName" type="text" class="form-control" id="eventName"
+                                        placeholder="Enter event name" required>
                                 </div>
-                                <div class="m-3">
-                                    <label for="eventArtist">Event Artist</label>
-                                    <input id="eventArtist" name="eventArtist" type="text" required />
+
+                                <div class="mb-3">
+                                    <label for="eventArtist" class="form-label">Event Artist</label>
+                                    <input name="eventArtist" type="text" class="form-control" id="eventArtist"
+                                        placeholder="Enter event name" required>
                                 </div>
-                                <div class="m-3">
-                                    <label for="eventDesc">Event Description</label>
-                                    <textarea id="eventDesc" name="eventDesc" placeholder="Insert Description Here..."
-                                        required></textarea>
-                                </div>
-                                <div class="m-3">
+
+                                <div class="mb-3">
                                     <label for="eventPhoto">Event Photo</label>
                                     <input id="eventPhoto" name="eventPhoto" type="file" required />
                                 </div>
-                                <div class="m-3">
-                                    <label for="eventDate">Event Date and Time</label>
-                                    <input id="eventDate" name="eventDate" type="datetime-local" required />
+
+                                <div class="mb-3">
+                                    <label for="eventDate" class="form-label">Event Date and Time</label>
+                                    <input class="form-control" id="eventDate" name="eventDate" type="datetime-local"
+                                        required />
                                 </div>
+
+                                <div class="mb-3">
+                                    <label for="eventDescription" class="form-label">Event Description</label>
+                                    <textarea class="form-control" name="eventDescription" id="eventDescription"
+                                        rows="4" placeholder="Enter event description" required></textarea>
+                                </div>
+
+                                
                                 <div class="text-center">
                                     <input class="my-3 btn btn-primary" name="submit" type="submit"
                                         value="Create Event">
