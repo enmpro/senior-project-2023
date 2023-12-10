6633f87e8d56c5ce8bd19cdf5662a36b28378584
Nicole <vierne@farmingdale.edu>
2023-12-10 15:40:45 -0500

    small change

--- send_request.php
+++ send_request.php
@@ -4,10 +4,9 @@ require_once 'logindb.php';
 try {
   $pdo = new PDO($attr, $user, $pass, $opts);
 } catch (PDOException $e) {
-  throw
- 
-new PDOException($e->getMessage(), (int) $e->getCode());
+  throw new PDOException($e->getMessage(), (int) $e->getCode());
 }
+
 
 session_start();
 if (!isset($_SESSION['user_name'])) {
