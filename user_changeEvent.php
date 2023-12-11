966a4bc71fd75813d9875e1d17a33c54f172b183
Enmanuel Proano <rbsrman123@gmail.com>
2023-12-11 13:58:42 -0500

    test

---
+++ user_changeEvent.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'logindb.php';
+
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int) $e->getCode());
+}
+
+session_start();
+if (!isset($_SESSION['user_name'])) {
+    // The user is not logged in, redirect them to the login page
+    header('Location: landing.html');
+    exit;
+}
+
+$userID = $_SESSION['user_id'];
+
+
+
+
+?>
