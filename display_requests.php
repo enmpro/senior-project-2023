9aa683d57af9087bb1bea4c7526143c42ee1fd52
Nicole <vierne@farmingdale.edu>
2023-12-12 11:04:17 -0500

    html fix

--- display_requests.php
+++ display_requests.php
@@ -29,30 +29,41 @@ try {
 }
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Pending Friend Requests</title>
+    <title>Friend Requests</title>
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
+<style>
+    /* Add custom styles here, if needed */
+    body {
+        padding-top: 100px;
+        /* Adjust for fixed navbar height */
+    }
+
+    .homepage-section {
+        padding: 60px 0;
+    }
+</style>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
             <a class="navbar-brand" href="#">CANTIO</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse text-center justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav ">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="homepage.php">Main</a>
+                        <a class="nav-link" aria-current="page" href="homepage.php">Main</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="profile.php">Profile</a>
@@ -61,7 +72,7 @@ try {
                         <a class="nav-link" href="/spotify/explore_page.php">Explore Music</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="community.php">Community</a>
+                        <a class="nav-link active" href="community.php">Community</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="user_event.php">Event</a>
@@ -77,7 +88,7 @@ try {
                     ?>
 
                 </ul>
-                <div>
+                <div class="my-3 mx-4">
                     <form method="post" action="user_logout.php">
                         <button class="btn btn-secondary" type="submit" name="logout">Log Out</button>
 
